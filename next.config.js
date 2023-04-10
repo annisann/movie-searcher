5a9f27c00c21ce43453ccd36d41742c734fa95e6
annisann <hello.annisann@gmail.com>
2023-04-11 00:17:36 +0700

    🔗 next config to load image from source

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com'
+      }
+    ]
+  },
   env: {
     API_KEY: process.env.API_KEY
   }
